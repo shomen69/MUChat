0db80b858798ef3641b61aa46d583748333357a7
Shomen <shomen69@gmail.com>
2016-01-31 16:19:54 +0600

    fourth

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const util = require('util');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var port = process.env.PORT || 3090;
 
 var users = {};
 users["123456"] = {name:"shomen",id:"shoemn_123",socket:{id:"123",name:"s"}};
@@ -9,3 +13,11 @@ users.name = "batman";
 console.log(util.inspect(users));
 console.log("value of users "+users);
 console.log("value of current date  "+new Date());
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
